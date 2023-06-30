fe8259aa525192c42c04ce4556645236277511d0
Lisa Schmierer <lisa.schmierer@tum.de>
2023-06-30 23:10:24 +0200

    add method to check node overlap and number of nan values

---
+++ sanity_checks.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+DATA_PATH_PPI = "./NBFNet/data/lnctard/train1-ppi.txt"
+DATA_PATH_TEST = "./NBFNet/data/lnctard/test.txt"
+PATH_ENTITY_TYPES = "/Users/lisa/Documents/Uni/ML4RG/linkpred/NBFNet/data/lnctard/entity_types_lnctardppi.txt"
+def check_node_overlap(path_ppi, path_file2check):
+    df_ppi = pd.read_csv(path_ppi,sep="\t", encoding="latin-1",names = ["end1", "end2"], usecols=[0,2]).drop_duplicates(ignore_index=True)
+    df_2check = pd.read_csv(path_file2check, sep="\t", names = ["end1", "end2"], usecols=[0,2])
+    df_ppi["index"], df_2check["index"] = df_ppi.index, df_2check.index
+    df_ppi['key'] = df_ppi[["end1", "end2"]].apply(lambda row: tuple(sorted(row)), axis=1)
+    df_2check['key'] = df_2check[["end1", "end2"]].apply(lambda row: tuple(sorted(row)), axis=1)
+    overlap = pd.merge(df_ppi, df_2check, how='inner', on="key")
+    return overlap
+
+def check_rel_distribution():
+    pass
+
+
+def check_single_node_type():
+    pass
+
+def count_nan(path, col_index):
+    df = pd.read_csv(path, sep="\t", usecols=[col_index])
+    return df.isna().sum()
+
+if __name__ == "__main__":
+    #overlap = check_node_overlap(DATA_PATH_PPI, DATA_PATH_TEST)
+    count_nan(PATH_ENTITY_TYPES, 1)
+
